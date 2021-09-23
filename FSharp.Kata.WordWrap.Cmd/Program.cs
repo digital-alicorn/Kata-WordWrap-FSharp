c32b5cb010629982c6684f2db957f8b3e2f7124d
Digital Alicorn <digital_alicorn@outlook.com>
2021-09-23 15:52:30 +0200

    Implement Solution

---
+++ FSharp.Kata.WordWrap.Cmd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FSharp.Kata.WordWrap.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Single Word Stays On 1 Line ->");
+            Console.WriteLine();
+            Console.WriteLine(Core.Do("Test", 80));
+            Console.WriteLine();
+            Console.WriteLine("Short Line Stays Unbroken ->");
+            Console.WriteLine();
+            Console.WriteLine(Core.Do("Test Test", 80));
+            Console.WriteLine();
+            Console.WriteLine("Long Text Gets Broken ->");
+            Console.WriteLine();
+            Console.WriteLine(Core.Do(@"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", 80));
+            Console.WriteLine();
+            Console.WriteLine("Single Long Word Gets Broken ->");
+            Console.WriteLine(Core.Do("Loremipsumdolorsitamet,consetetursadipscingelitr,seddiamnonumyeirmodtemporinviduntutlaboreetdoloremagna", 20));
+            Console.WriteLine();
+            Console.WriteLine("Long Word In Text Gets Broken ->");
+            Console.WriteLine(Core.Do("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, Loremipsumdolorsitamet,consetetursadipscingelitr,seddiamnonumyeirmodtemporinviduntutlaboreetdoloremagna Lorem ipsum dolor sit amet, consetetur sadipscing elitr,", 20));
+        }
+    }
+}
---
+++ FSharp.Kata.WordWrap/Core.fs
@@ -0,0 +1,17 @@
+﻿module Core
+
+let rec DoRec (words:list<string>, maxCharPerLine:int, charsOnLine:int, result:string) =
+    match words with
+    | [] -> result
+    | [h] when h.Length > maxCharPerLine -> DoRec ([h.Substring(0, maxCharPerLine);h.Substring(maxCharPerLine)], maxCharPerLine, charsOnLine, result)
+    | [h] when h.Length + charsOnLine = maxCharPerLine -> $"{result}{h}\n"
+    | [h] when h.Length + charsOnLine < maxCharPerLine -> $"{result}{h}"
+    | [h] -> DoRec ([h], maxCharPerLine, 0, $"{result}\n")
+    | h::t when h.Length > maxCharPerLine -> DoRec ([h.Substring(0, maxCharPerLine);h.Substring(maxCharPerLine)] @ t, maxCharPerLine, charsOnLine, result)
+    | h::t when h.Length + charsOnLine = maxCharPerLine -> DoRec (t, maxCharPerLine, 0, $"{result}{h}\n")
+    | h::t when h.Length + charsOnLine + 1 < maxCharPerLine -> DoRec (t, maxCharPerLine, (charsOnLine + h.Length + 1), $"{result}{h} ")
+    | h::t -> DoRec (h::t, maxCharPerLine, 0, $"{result}\n")
+
+let Do (text : string, maxCharPerLine : int) = 
+    let words = text.Split(' ') |> List.ofSeq
+    DoRec (words, maxCharPerLine, 0, "")
